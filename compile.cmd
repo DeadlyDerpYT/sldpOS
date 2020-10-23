52017b2954d2f6eeb60497df9e2457d2d7985404
DeadlyDerpYT <deathblocks05@gmail.com>
2020-10-22 18:29:43 -0700

    Fixing compiler Issues
    Fixing compiler issues with the Compile.cmd file

--- compile.cmd
+++ compile.cmd
@@ -2,11 +2,12 @@
 cd Assembly
 nasm.exe -f bin boot.asm -o boot.bin
 nasm.exe -f bin ExtendedProgram.asm -o ExtendedProgram.bin
-move boot.bin "C:\Users\D\Desktop\sldpOS"
-move ExtendedProgram.bin "C:\Users\D\Desktop\sldpOS"
+pause
+move boot.bin "C:\Users\D\Desktop\sldpOS\sldpOS"
+move ExtendedProgram.bin "C:\Users\D\Desktop\sldpOS\sldpOS"
 pause
 cd ..
 copy /b boot.bin+ExtendedProgram.bin bootloader.bin
 pause
 cd "C:\Program Files\qemu"
-qemu-system-x86_64 "C:\Users\D\Desktop\sldpOS\bootloader.bin"
+qemu-system-x86_64 -fda "C:\Users\D\Desktop\sldpOS\sldpOS\bootloader.bin"
